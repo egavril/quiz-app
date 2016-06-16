51192c14a1c55cd11bed79e483a17873015cc6c2
egavril <e.gavrilyuk@gmail.com>
2016-06-15 17:10:29 -0700

    styles adjustment and JS frustration

---
+++ js/app.js
@@ -0,0 +1,35 @@
+$(document).ready(function() {
+
+	//question objects array
+
+	var questionsContent = [{
+		question: "Question 1?",
+		choices: ["Answer 1a", "Answer 2", "Answer 3", "Answer 4", "Answer 5"],
+		correct: 1
+		},
+
+		{
+		question: "Question 2?",
+		choices: ["Answer 1b", "Answer 2", "Answer 3", "Answer 4", "Answer 5"],
+		correct: 4
+		},
+
+		{
+		question: "Question 3?",
+		choices: ["Answer 1c", "Answer 2", "Answer 3", "Answer 4", "Answer 5"],
+		correct: 1
+		},	
+
+		{
+		question: "Question 4?",
+		choices: ["Answer 1d", "Answer 2", "Answer 3", "Answer 4", "Answer 5"],
+		correct: 2
+		},	
+
+		{
+		question: "Question 5?",
+		choices: ["Answer 1e", "Answer 2", "Answer 3", "Answer 4", "Answer 5"],
+		correct: 0
+		}]
+
+	
